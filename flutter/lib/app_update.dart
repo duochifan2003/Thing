1b93f14da335ba8f159d83abc4d1213fe1350321
duochifan2003 <duochifan2003@gmail.com>
2026-08-03 03:13:32 +0800

    格式化更新服务代码

--- flutter/lib/app_update.dart
+++ flutter/lib/app_update.dart
@@ -4,7 +4,10 @@ import 'dart:io' as io;
 
 import 'package:path/path.dart' as path;
 
-const appVersion = String.fromEnvironment('APP_VERSION', defaultValue: '0.1.10');
+const appVersion = String.fromEnvironment(
+  'APP_VERSION',
+  defaultValue: '0.1.10',
+);
 const appBuild = String.fromEnvironment('APP_BUILD', defaultValue: '33');
 const appVersionLabel = 'v$appVersion+$appBuild';
 
